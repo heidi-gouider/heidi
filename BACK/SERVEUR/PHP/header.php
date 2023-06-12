2a6ecbbe0af0ad7458eee621169e54615f7de387
gouider heidi <gouider.heidi@gmail.com>
2023-06-12 16:50:57 +0200

    création fichiers php pour le header et le footer

---
+++ BACK/SERVEUR/PHP/header.php
@@ -0,0 +1,17 @@
+<header>
+        <nav>
+            <img class="logo" src="../ASSETS/IMG/logo/logo.png" alt="logo">
+
+            <div class="menu">
+                <a href="index.html">Accueil</a>
+                <a href="category.html">Catégories</a>
+                <a href="plats.html">Plats</a>
+                <a href="contact.html">Contact</a>
+            </div>
+
+            <div class="icone">
+                <span class="open"></span>
+                <span class="close"></span>
+            </div>
+        </nav>
+    </header>

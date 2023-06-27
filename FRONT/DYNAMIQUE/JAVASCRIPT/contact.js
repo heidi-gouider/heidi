357b115ed3be9a40c4f3ade413a763467dab3a82
gouider heidi <gouider.heidi@gmail.com>
2023-06-27 16:49:47 +0200

    modif affichage modal Jquery

--- FRONT/DYNAMIQUE/JAVASCRIPT/contact.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/contact.js
@@ -4,18 +4,33 @@
 //***  variables ***//
 
 // je récupère mon formulaire
+
+
+
 let form = document.querySelector(".validation");
+
+
 
 
 //***  évennement / fonction / classe ***//
 
 // j'ajoute un écouteur sur le formulaire pour l'évennement "submit"
-form.addEventListener("submit", valider);
+
+
+// document.querySelector("validation").addEventListener("submit", valider);
+
+
+
 // la fonction "valider (e)" est appelée quand la soumission du formulaire est déclenchée
 // j'utilise la méthode "checkValidity()" pour vérifier la validité du formulaire
 // et la méthode "preventDefault()" pour empêcher l'envoi du formulaire si la validatin échoue
 // j'ajoute la class bootstrap "was-validated" pour activer les styles de validation propre à la bibliothèque bs
 // J'utilise la fonction "modal()" de jQuery pour afficher une modal
+
+// $('#valid').on('click',valider);
+$('.validation').on('click',valider);
+
+
 function valider(e) {
     if (form.checkValidity() == false) {
         e.preventDefault();
@@ -25,3 +40,4 @@ function valider(e) {
     form.classList.add("was-validated");
 }
 
+//il faut faire en sorte que l'event se declanche au click button et pas sur le onclick de la zone "votre message"

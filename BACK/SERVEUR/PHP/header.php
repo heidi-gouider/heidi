911e56865ecc5dad8f1efa80cf90d28e7f5d160e
gouider heidi <gouider.heidi@gmail.com>
2023-07-04 16:19:28 +0200

    modif orga

--- BACK/SERVEUR/PHP/header.php
+++ BACK/SERVEUR/PHP/header.php
@@ -12,10 +12,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Accuei</title>
   <meta name="description" content="">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-  integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"/>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-  <!-- <link rel="stylesheet" type="text/css" href="../CSS/plats.css" /> -->
 </head>
 <body>
     <div class="container">
@@ -62,5 +58,34 @@
             </div>
         </nav>
     </div>
+
+    <?php
+      $euro=6.55957;
+      printf("%.2f FF<br />",$euro);
+      $money1 = 68.75;
+      $money2 = 54.35;
+      $money = $money1 + $money2;
+      // echo $money affichera "123.1";
+      echo "affichage sans printf : " . $money . "<br />";
+      $monformat = sprintf("%01.2f", $money);
+
+      // echo $monformat affichera "123.10"
+      echo "affichage avec printf : " . $monformat . "<br />";
+
+      $year="2002";
+      $month="4";
+      $day="5";
+      $varDate = sprintf("%04d-%02d-%02d", $year, $month, $day) ;
+
+      // echo $varDate affichera "2002-04-05"
+      echo "affichage avec sprintf :\n" . $varDate. "<br />";
+      $var1 = "bonjour";
+$$var1 = "le monde"; 
+echo $bonjour. "<br />";
+echo $_SERVER["REMOTE_ADDR"]. "<br />";
+// echo $_SERVER["SERVER_ADDR"];
+
+    ?>
+    
 </body>
 </html>

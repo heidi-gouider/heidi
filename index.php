0e7cb27816f7efa7f9c127df8698cfa21204c24c
heidi gouider <gouider.heidi@gmail.com>
2023-07-19 19:46:27 +0200

    suppr de comments

--- index.php
+++ index.php
@@ -3,32 +3,20 @@ $title = "Accueil";
 include('partials/header.php');
 ?>
 
-<!-- <div class="container-fluid video-container"> -->
 <div class="row video-container">
     <div class="text-white" id="titre-accueil">
         <h1>The District</h1>
     </div>
 
-    <!-- <div class="row justify-content-center"> -->
     <form class="search-form d-flex justify-content-center" id="search-bar">
-          <input type="text" placeholder="Rechercher...">
-          <button class="btn text-light bg-dark" type="submit"><i class="bi bi-search"></i></button>
-        </form>
-    <!-- </div> -->
-    <!-- <div class="row justify-content-center"> -->
-        <!-- <div class="input-group search-bar w-25">-->
-        <!-- <div class="col-3 col-md-4 col-lg-4 search-bar " id="search-bar">
-            <input class="form-control" type="search" placeholder="Recherche" aria-label="Search">
-            <button class="btn text-light bg-dark"><i class="bi bi-search"></i></button>
-        </div>
-    </div> -->
+        <input type="text" placeholder="Rechercher...">
+        <button class="btn text-light bg-dark" type="submit"><i class="bi bi-search"></i></button>
+    </form>
     <video controls="controls" class="" loop="loop" autoplay="autoplay" muted>
         <!-- <video controls="controls" class="controls video-fluid" loop="loop" autoplay="autoplay" muted> -->
         <source src="public/VIDEO/video2.mp4">
     </video>
 </div>
-
-<!-- <h2>Bienvenue au restaurant</h2> -->
 
 <div class="container d-grid mt-lg-4" id="images">
     <div class="row gap-5 justify-content-sm-center m-3">

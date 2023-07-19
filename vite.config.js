22bb445cea45739c3ba561495c88499868f1f803
heidi gouider <gouider.heidi@gmail.com>
2023-07-19 19:47:00 +0200

    ajout de test code pour pb icon bs

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 // pour supprimer les hash lors du build avec yarn sur le nom des fichiers renomés
-import{defineConfig} from 'vite';
+import { defineConfig } from 'vite';
+import path from 'path';
+
+
 export default defineConfig({
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: `assets/[name].js`,
-          chunkFileNames: `assets/[name].js`,
-          assetFileNames: `assets/[name].[ext]`
-        }
-      }
-    }
-  })
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      },
+    },
+  },
+  // test de solution pour le problème de récupération des icones bs
+  root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    },
+  },
+
+});

e0584dd13d921aea191c3da87b1b470cd29c4662
gouider heidi <gouider.heidi@gmail.com>
2023-07-27 11:54:12 +0200

    carousel

--- PAGES/carousel.php
+++ PAGES/carousel.php
@@ -1,45 +1,34 @@
 <?php
-$title="category";
-include ('../partials/header.php');
+$title = "category";
+include('../partials/header.php');
 ?>
 
-  <div class="contenu">
+<div class="contenu">
     <h1 class="fst-italic">Notre carte</h1>
     <section class="category">
-        <div class="main d-flex align-items-center justify-content-center m-3">
-            <!-- <div id="carousel" class="carousel slide" data-ride="carousel"> -->
-            <div class="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <!-- <div class="carousel-item active zoomed-item"> -->
-                        <!-- <div class="row gap-3 justify-content-center"> -->
-                        <!-- <div class="col-md-2"> -->
-                        <img src="/public/IMG/category/burger_cat.jpg" class="rounded w-25" alt="photo burger" />
-                        <!-- </div> -->
-                        <!-- <div class="col-md-2"> -->
-                        <img src="/public/IMG/category/asian_food_cat.jpg" class="rounded w-25" id="image2"
-                            alt="photo asian food" />
-                        <!-- </div> -->
-                        <!-- <div class="col-md-2"> -->
-                        <img src="/public/IMG/category/pizza_cat.jpg" class="rounded w-25" id="image3"
-                            alt="photo pizza" />
-                        <!-- </div> -->
-                        <!-- </div> -->
-                        <!-- </div> -->
-
-                        <!-- <div class="carousel-item"> -->
-                        <!-- <div class="row gap-3 justify-content-center"> -->
-                        <!-- <div class="col-md-3"> -->
-                        <img src="/public/IMG/category/burger_cat.jpg" class="rounded w-25" alt="photo burger" />
-                        <!-- </div> -->
-                        <!-- <div class="col-md-3"> -->
-                        <img src="/ASSETS/IMG/category/pasta_cat.jpg" class="rounded w-25" alt="pates" />
-                        <!-- </div> -->
-                        <!-- <div class="col-md-3"> -->
-                        <img src="/public/IMG/category/salade_cat.jpg" class="rounded w-25" alt="photo salades" />
-                    </div>
+        <!-- conteneur principal -->
+        <div class="carousel d-flex align-items-center justify-content-center mx-auto">
+            <!-- <div class="carousel"> -->
+            <!-- conteneur des éléments -->
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <!-- <div class="image-container"> -->
+                        <img src="/public/IMG/category/burger_cat.jpg" class="rounded p-3 " alt="photo burger">
+                        <!-- <div class="carousel-caption d-none d-md-block text-white text-center text-wrap custom-scrollbar">
+                            <h2>Des savouveux burgers</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident iste ratione illum,
+                                consequatur unde dolor sequi est minima odio debitis explicabo praesentium voluptas reiciendis iure illo dignissimos atque libero. Consequatur.
+                            </p>
+                        </div> -->
+                    <!-- </div> -->
+                    <img src="/public/IMG/category/asian_food_cat.jpg" class="rounded p-3" id="image2" alt="photo asian food">
+                    <img src="/public/IMG/category/pizza_cat.jpg" class="rounded p-3" id="image3" alt="photo pizza">
+                    <img src="/public/IMG/category/burger_cat.jpg" class="rounded p-3" alt="photo burger">
+                    <img src="/public/IMG/category/pasta_cat.jpg" class="rounded p-3" alt="pates">
+                    <img src="/public/IMG/category/salade_cat.jpg" class="rounded p-3" alt="photo salades">
                 </div>
             </div>
+            <!-- </div> -->
         </div>
         <!-- </div> -->
         <!-- CONTROL DU CAROUSEL -->
@@ -47,16 +36,14 @@ include ('../partials/header.php');
 
 
     <button class="carousel-control-prev" href="#carousel" type="button" data-bs-slide="prev">
-        <svg id="i-caret-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none"
-            stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+        <svg id="i-caret-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
             <path d="M22 30 L6 16 22 2 Z" />
         </svg>
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only"></span>
     </button>
     <button class="carousel-control-next" href="#carousel" type="button" data-bs-slide="next">
-        <svg id="i-caret-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32"
-            fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+        <svg id="i-caret-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
             <path d="M10 30 L26 16 10 2 Z" />
         </svg>
         <!-- <span class="carousel-control-next-icon" aria-hidden="true"></span> -->
@@ -88,13 +75,13 @@ include ('../partials/header.php');
                       </button>
                     </div>
                   </div> -->
-  </div>
+</div>
 
-  <?php
+<?php
 include('../partials/footer.php');
 ?>
-
-    <script type="module" src="../../../main.js"></script>
+<script src="/JAVASCRIPT/Carousel.js"></script>
+<script type="module" src="../../../main.js"></script>
 
 </body>
 

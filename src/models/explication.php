1212150741ed216eae61fc61853dcda6280c684c
gouider heidi <gouider.heidi@gmail.com>
2023-09-18 11:56:13 +0200

    ajout comment mvc

--- src/models/explication.php
+++ src/models/explication.php
@@ -1,2 +1,2 @@
-<!-- Le dossier "Models" peut contenir vos fichiers PHP qui représentent les modèles de données de votre application.
+<!-- Le dossier "Models" peut contenir les fichiers PHP qui représentent les modèles de données de l'application.
 Cela inclut généralement les interactions avec la base de données, les requêtes SQL, etc. -->
---
+++ src/views/explication.php
@@ -0,0 +1,9 @@
+<!-- Le dossier "Views" du côté backend (PHP) contient des fichiers de vue PHP.
+Ces fichiers de vue PHP sont responsables de la génération dynamique de contenu HTML en utilisant les données fournies par les contrôleurs.
+
+Par exemple, pour une page de liste de produits, un contrôleur PHP peut récupérer la liste des produits à partir de la base de données,
+puis utiliser un fichier de vue PHP pour générer la partie dynamique de la page en insérant les données des produits dans le modèle HTML.
+Le résultat final est une page HTML complète avec des données dynamiques.
+
+Les fichiers de vue PHP sont particulièrement utiles pour générer des parties complexes de la page qui dépendent des données ou de la logique métier.
+Ils permettent de garder le code PHP responsable de la génération du contenu, tandis que le modèle HTML reste principalement statique. -->

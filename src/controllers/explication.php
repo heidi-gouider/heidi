80eabd58ce96e308798bf194862de42cb8bbfaa1
gouider heidi <gouider.heidi@gmail.com>
2023-10-11 16:51:01 +0200

    erreur mise en ligne

--- src/controllers/explication.php
+++
@@ -1,2 +0,0 @@
-<!-- Dans le dossier "Controllers", placez vos fichiers PHP qui gèrent la logique métier de votre application. Chaque contrôleur peut être responsable d'une partie spécifique de votre site,
-par exemple, un contrôleur pour la gestion des produits, un autre pour la gestion des utilisateurs, etc. -->
--- src/models/explication.php
+++
@@ -1,2 +0,0 @@
-<!-- Le dossier "Models" peut contenir les fichiers PHP qui représentent les modèles de données de l'application.
-Cela inclut généralement les interactions avec la base de données, les requêtes SQL, etc. -->
